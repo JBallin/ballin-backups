1f2e41d37031316ad386b17201dd97ea146168eb
JBallin <JBallin@users.noreply.github.com>
2018-08-18 22:02:16 -0700

    Store all categories in state

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+const API = process.env.REACT_APP_API;
 
 class App extends Component {
+  state = {files: [], categories: []};
+
+  componentDidMount = async () => {
+    const categoriesRes = await fetch(API + '/categories');
+    const categories = await categoriesRes.json();
+
+    this.setState({files, categories});
+  }
+
   render() {
     return (
       <div className="App">
