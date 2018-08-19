3b2c9db04d8810414036e595dd1442c2f09e4777
JBallin <JBallin@users.noreply.github.com>
2018-08-18 22:02:52 -0700

    Store all files from gist_id in state

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 const API = process.env.REACT_APP_API;
+const gistAPI = 'https://api.github.com/gists'
 
 class App extends Component {
   state = {files: [], categories: []};
 
   componentDidMount = async () => {
+    const usersRes = await fetch(API + '/users');
+    const users = await usersRes.json();
+    const gist_id = users[0].gist_id;
+    const gistRes = await fetch(gistAPI + '/' + gist_id)
+    const gist = await gistRes.json();
+    const { files } = gist;
+
     const categoriesRes = await fetch(API + '/categories');
     const categories = await categoriesRes.json();
 
