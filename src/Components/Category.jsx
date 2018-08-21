a1238b1f17bb4912a9db7d0c943a45f7a85527f7
JBallin <JBallin@users.noreply.github.com>
2018-08-21 00:51:44 -0700

    Implement toggleDisplay

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../css/Category.css';
 import File from './File';
 
-const Category = ({ category, gistID }) => {
-  const title = Object.keys(category)[0];
-  const files = Object.values(category)[0];
-  return (
-    <div>
-      <button className={'category'} onClick={() => window.location.href=`#${title}`}>
-        <h3>{title}</h3>
-      </button>
-      {files.map(({title, extension}, i) => (
-        <File title={title} extension={extension} gistID={gistID} key={i} />
-      ))}
-    </div>
-  )
+class Category extends Component {
+  state = {display: false};
+
+  toggleDisplay = () => {
+    this.setState(prev => ({display: !prev.display}))
+  }
+
+  render() {
+    const { category, gistID } = this.props;
+    const title = Object.keys(category)[0];
+    const files = Object.values(category)[0];
+    return (
+      <div>
+        <button className={'category'} onClick={this.toggleDisplay}>
+          <h3>{title}</h3>
+        </button>
+        {this.state.display && files.map(({title, extension}, i) => (
+          <File title={title} extension={extension} gistID={gistID} key={i} />
+        ))}
+      </div>
+    )
+  };
 }
 
 export default Category;
