de6cd64f295bd4d9fb2a0a2f1ec89912214da726
JBallin <JBallin@users.noreply.github.com>
2018-08-21 01:03:17 -0700

    Fix most linter warnings

--- src/Components/GistFile.jsx
+++ src/Components/GistFile.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Gist from 'react-gist';
 import '../css/File.css';
 
-const GistFile = ({gistID, title, extension}) => {
+const GistFile = ({ gistID, title, extension }) => {
   const fileName = title + extension;
   return (
     <div>
-      <h5>{title}</h5>
-      <div className={'gist'}>
+      <h5>
+        {title}
+      </h5>
+      <div className="gist">
         <Gist id={gistID} file={fileName} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default GistFile;
