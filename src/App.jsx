3732f3971223e6cdda0eab2b80b42e31838d6204
JBallin <JBallin@users.noreply.github.com>
2018-11-17 16:14:11 -0800

    Create AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'reactstrap';
-import Categories from './components/Categories';
-import Login from './components/Login';
+import AppRouter from './AppRouter';
 import TopNav from './components/TopNav';
 
-class App extends Component {
-  state = {
-    isLoggedIn: false,
-  }
-
-  attemptLogin = (username, password) => {
-    console.log('username', username);
-    console.log('password', password);
-  }
-
-  render() {
-    const { isLoggedIn } = this.state;
-    return (
-      <div>
-        <TopNav title="My Sweet Config" />
-        <Container>
-          {
-            !isLoggedIn
-              ? <Login attemptLogin={this.attemptLogin} />
-              : <Categories />
-          }
-        </Container>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <TopNav title="My Sweet Config" />
+    <Container>
+      <AppRouter />
+    </Container>
+  </div>
+);
 
 export default App;
---
+++ src/AppRouter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Signup from './components/Signup';
+import UserProfile from './components/UserProfile';
+import NoMatch from './components/NoMatch';
+import SignupHelp from './components/SignupHelp';
+
+const AppRouter = () => (
+  <Switch>
+    <Route path="/" exact render={() => <Redirect to="/login" />} />
+    <Route path="/login" component={Login} />
+    <Route path="/logout" component={Logout} />
+    <Route path="/signup/help" component={SignupHelp} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/profile" component={UserProfile} />
+    <Route component={NoMatch} />
+  </Switch>
+);
+
+export default AppRouter;
