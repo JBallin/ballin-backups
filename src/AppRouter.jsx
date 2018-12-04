622d266514b5f3e1740d2c0720cd45755685e9fa
JBallin <JBallin@users.noreply.github.com>
2018-12-04 12:51:42 -0800

    [AppRouter] always check auth (/auth) and sitedown (/site-down)

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import {
+  Route, Redirect, Switch, withRouter,
+} from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -9,6 +11,8 @@ import Signup from './components/Signup';
 import UserProfile from './components/UserProfile';
 import NoMatch from './components/NoMatch';
 import SignupHelp from './components/SignupHelp';
+import Auth from './components/Auth';
+import SiteDown from './components/SiteDown';
 
 class AppRouter extends React.Component {
   static propTypes = {
@@ -30,6 +34,30 @@ class AppRouter extends React.Component {
     const { location } = this.props;
     this.setState({ initialPath: location.pathname });
   }
+
+  render() {
+    const { user, tokenLoginFailure, APIFetchFailure } = this.props;
+    const { initialPath } = this.state;
+    const checkingAuth = !user.username && !tokenLoginFailure && !APIFetchFailure;
+    return (
+      <Switch>
+        <Route
+          path="/auth"
+          render={() => <Auth prevPath={initialPath} />}
+        />
+        { checkingAuth && <Redirect to="/auth" /> }
+        <Route path="/site-down" component={SiteDown} />
+        { APIFetchFailure && <Redirect to="/site-down" /> }
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/signup/help" component={SignupHelp} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/profile" component={UserProfile} />
+        <Route component={NoMatch} />
+      </Switch>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   user: state.auth.user,
