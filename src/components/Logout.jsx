cb7e31ee4c536fa757da885d17284bf7c182e6cc
JBallin <JBallin@users.noreply.github.com>
2018-12-29 14:48:54 -0800

    [logout] add sweet alert farewell on success

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Swal from 'sweetalert2';
 import { userLogout } from '../redux/actions/auth.actions';
 
 class Logout extends React.Component {
@@ -10,17 +11,32 @@ class Logout extends React.Component {
     userLogoutAction: PropTypes.func.isRequired,
     showLogoutError: PropTypes.bool.isRequired,
     errorMessage: PropTypes.string.isRequired,
+    username: PropTypes.string,
   };
+
+  static defaultProps = {
+    username: '',
+  }
 
   state = {
     attemptedLogout: false,
+    username: '',
   }
 
   async componentDidMount() {
-    const { userLogoutAction } = this.props;
+    const { userLogoutAction, username } = this.props;
     await userLogoutAction();
-    this.setState({ attemptedLogout: true });
+    this.setState({ attemptedLogout: true, username });
   }
+
+  alertSuccessAndRedirect = () => {
+    const { username } = this.state;
+    Swal({
+      type: 'success',
+      title: `<p>Bye <i>${username}</i>!</p>`,
+    });
+    return <Redirect push to="/login" />;
+  };
 
   render() {
     document.title = 'Logout | My Sweet Config';
@@ -28,7 +44,7 @@ class Logout extends React.Component {
     const { showLogoutError, errorMessage } = this.props;
     if (attemptedLogout) {
       if (showLogoutError) return <h1>{errorMessage}</h1>;
-      return <Redirect push to="/login" />;
+      return this.alertSuccessAndRedirect();
     }
     return null;
   }
@@ -36,6 +52,7 @@ class Logout extends React.Component {
 
 const mapStateToProps = state => ({
   showLogoutError: state.auth.showLogoutError,
+  username: state.auth.user.username,
   errorMessage: state.auth.errorMessage,
 });
 
