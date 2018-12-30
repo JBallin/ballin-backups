275ec0b25528709278d33bf047b9ba28cec936da
JBallin <JBallin@users.noreply.github.com>
2018-12-29 17:13:41 -0800

    Combine userUpdate and userDelete reducers with rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import auth from './auth.reducers';
 import signup from './signup.reducers';
 import api from './api.reducers';
+import userUpdate from './userUpdate.reducers';
+import userDelete from './userDelete.reducers';
 
 const rootReducer = combineReducers({
   auth,
   signup,
   api,
+  userUpdate,
+  userDelete,
 });
 
 export default rootReducer;
